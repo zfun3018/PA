b66ce1a5f0e810670e538cb65879443ca6ebf17d
zfun3018 <61958029+zfun3018@users.noreply.github.com>
2026-02-01 13:01:21 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "zfun3018.github.io/PA",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
